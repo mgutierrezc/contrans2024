92a30220e6e880692c0eb9a1a2c46938edd49d74
Marco G <a20141676@pucp.pe>
2024-10-23 21:29:28 -0400

    added make_members_df function

--- contrans.py
+++ contrans.py
@@ -1,6 +1,7 @@
 import dotenv, os
 import pandas as pd
 import requests, json
+from bs4 import BeautifulSoup
 
 dotenv.load_dotenv()
 
@@ -15,6 +16,27 @@ class contrans:
         self.mypassword = os.getenv("mypassword")
         self.congresskey = os.getenv("congresskey")
         self.newskey = os.getenv("newskey")
+        self.us_state_to_abbrev = {
+                "Alabama": "AL","Alaska": "AK","Arizona": "AZ","Arkansas": "AR",
+                "California": "CA","Colorado": "CO","Connecticut": "CT","Delaware": "DE",
+                "Florida": "FL","Georgia": "GA","Hawaii": "HI",
+                "Idaho": "ID","Illinois": "IL","Indiana": "IN","Iowa": "IA",
+                "Kansas": "KS","Kentucky": "KY","Louisiana": "LA",
+                "Maine": "ME","Maryland": "MD","Massachusetts": "MA",
+                "Michigan": "MI","Minnesota": "MN","Mississippi": "MS",
+                "Missouri": "MO","Montana": "MT","Nebraska": "NE",
+                "Nevada": "NV","New Hampshire": "NH","New Jersey": "NJ",
+                "New Mexico": "NM","New York": "NY","North Carolina": "NC",
+                "North Dakota": "ND","Ohio": "OH","Oklahoma": "OK",
+                "Oregon": "OR","Pennsylvania": "PA","Rhode Island": "RI",
+                "South Carolina": "SC","South Dakota": "SD","Tennessee": "TN",
+                "Texas": "TX","Utah": "UT","Vermont": "VT",
+                "Virginia": "VA","Washington": "WA","West Virginia": "WV",
+                "Wisconsin": "WI","Wyoming": "WY","District of Columbia": "DC",
+                "American Samoa": "AS","Guam": "GU","Northern Mariana Islands": "MP",
+                "Puerto Rico": "PR","United States Minor Outlying Islands": "UM",
+                "Virgin Islands": "VI"
+                }
 
     def get_votes(self):
         """
@@ -141,3 +163,86 @@ class contrans:
             j = j + 250
 
         return bills_list
+
+    def get_billdata(self, billurl):
+        r = requests.get(billurl,
+                            params={"api_key": self.congresskey})
+        bill_json = json.loads(r.text)
+        texturl = bill_json["bill"]["textVersions"]["url"]
+        r = requests.get(texturl,
+                            params={"api_key": self.congresskey})
+        toscrape = json.loads(r.text)["textVersions"][0]["formats"][0]["url"]
+        r = requests.get(toscrape)
+        mysoup = BeautifulSoup(r.text, "html.parser")
+        billtext = mysoup.text 
+        bill_json["bill_text"] = billtext
+        return bill_json
+    
+    def make_cand_table(self, members):
+        """
+        member is output of get terms
+        """
+        
+        replace_map = {"Republican": "R", "Democratic": "D", "Independent": "I"}
+        members["partyletter"] = members["partyName"].replace(replace_map)
+        members["state"] = members["state"].replace(self.us_state_to_abbrev)
+        members["district"] = members["district"].fillna(0)
+        members["district"] = members["district"].astype("int").astype("str")
+        members["district"] = ["0" + x if len(x) == 1 else x for x in members["district"]]
+        members["district"] = [x.replace("00", "S") for x in members["district"]]
+        members["DistIDRunFor"] = members["state"] + members["district"]
+        members["lastname"] = [x.split(",")[0] for x in members["name"]]
+        members["firstname"] = [x.split(",")[1] for x in members["name"]]
+        members["name2"] = [y.strip() + " (" + z.strip() + ")" 
+                            for y, z in 
+                            zip(members["lastname"], members["partyletter"])]
+        
+        cands = pd.read_csv("data/CampaignFin22/cands22.txt", quotechar="|", header=None)
+        cands.columns = ["Cycle", "FECCandID", "CID", "FirstLastP",
+                            "Party", "DistIDRunFor", "DistIDCurr",
+                            "CurrCand", "CycleCand", "CRPICO", "RecipCode", "NoPacs"]
+        cands["DistIDRunFor"] = [x.replace("S0", "S") for x in cands["DistIDRunFor"]]
+        cands["DistIDRunFor"] = [x.replace("S1", "S") for x in cands["DistIDRunFor"]]
+        cands["DistIDRunFor"] = [x.replace("S2", "S") for x in cands["DistIDRunFor"]]
+        cands["name2"] = [" ".join(x.split(" ")[-2:]) for x in cands["FirstLastP"]]
+        cands = cands[["CID", "name2", "DistIDRunFor"]].drop_duplicates(subset=["name2", "DistIDRunFor"])
+        crosswalk = pd.merge(members, cands, 
+                                left_on=["name2", "DistIDRunFor"],
+                                right_on=["name2", "DistIDRunFor"],
+                                how="left")
+        return crosswalk
+
+    def terms_df(self, members):
+        termsDF = pd.DataFrame()
+        for index, row in members.iterrows():
+            bioguide_id = row["bioguideId"]
+            terms = row["terms.item"]
+            df = pd.DataFrame.from_records(terms)
+            df["bioguideId"] = bioguide_id
+            termsDF = pd.concat([termsDF, df])
+        members = members.drop("terms.item", axis=1)
+        return termsDF, members
+    
+    def make_members_df(self, members, ideology):
+        """
+        Members should be the output of  get_bioguideIDs(),
+        with terms removed by get_terms()
+        augmented with contributions by make_cand_table(),
+        ideology should be the output of get_ideology()
+        """
+
+        members_df = pd.merge(members, ideology,
+                              left_on="bioguideId",
+                              right_on="bioguide_id",
+                              how="left")
+        
+        return members_df
+
+    def make_terms_df(self):
+                return self
+        
+    def make_votes_df(self):
+            return self
+    
+    def make_agreement_df(self):
+            return self
